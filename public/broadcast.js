5f91b7586521c0a34adb7ffdc6cf194e18d4bc12
Julian Jelfs <julian.jelfs@gmail.com>
2021-11-18 12:16:01 +0000

    adding stun and turn

--- public/broadcast.js
+++ public/broadcast.js
@@ -1,15 +1,27 @@
 const peerConnections = {};
 const config = {
   iceServers: [
-    { 
-      "urls": "stun:stun.l.google.com:19302",
+    {
+      urls: "stun:testnet.dfinity.network:5349",
+      username: "anon",
+      credential: "anon",
     },
-    // { 
-    //   "urls": "turn:TURN_IP?transport=tcp",
-    //   "username": "TURN_USERNAME",
-    //   "credential": "TURN_CREDENTIALS"
-    // }
-  ]
+    {
+      urls: "turn:testnet.dfinity.network:5349",
+      username: "anon",
+      credential: "anon",
+    },
+  ],
+  // iceServers: [
+  //   {
+  //     urls: "stun:stun.l.google.com:19302",
+  //   },
+  //   // {
+  //   //   "urls": "turn:TURN_IP?transport=tcp",
+  //   //   "username": "TURN_USERNAME",
+  //   //   "credential": "TURN_CREDENTIALS"
+  //   // }
+  // ],
 };
 
 const socket = io.connect(window.location.origin);
@@ -18,14 +30,14 @@ socket.on("answer", (id, description) => {
   peerConnections[id].setRemoteDescription(description);
 });
 
-socket.on("watcher", id => {
+socket.on("watcher", (id) => {
   const peerConnection = new RTCPeerConnection(config);
   peerConnections[id] = peerConnection;
 
   let stream = videoElement.srcObject;
-  stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+  stream.getTracks().forEach((track) => peerConnection.addTrack(track, stream));
 
-  peerConnection.onicecandidate = event => {
+  peerConnection.onicecandidate = (event) => {
     if (event.candidate) {
       socket.emit("candidate", id, event.candidate);
     }
@@ -33,7 +45,7 @@ socket.on("watcher", id => {
 
   peerConnection
     .createOffer()
-    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then((sdp) => peerConnection.setLocalDescription(sdp))
     .then(() => {
       socket.emit("offer", id, peerConnection.localDescription);
     });
@@ -43,7 +55,7 @@ socket.on("candidate", (id, candidate) => {
   peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
 });
 
-socket.on("disconnectPeer", id => {
+socket.on("disconnectPeer", (id) => {
   peerConnections[id].close();
   delete peerConnections[id];
 });
@@ -60,9 +72,7 @@ const videoSelect = document.querySelector("select#videoSource");
 audioSelect.onchange = getStream;
 videoSelect.onchange = getStream;
 
-getStream()
-  .then(getDevices)
-  .then(gotDevices);
+getStream().then(getDevices).then(gotDevices);
 
 function getDevices() {
   return navigator.mediaDevices.enumerateDevices();
@@ -85,7 +95,7 @@ function gotDevices(deviceInfos) {
 
 function getStream() {
   if (window.stream) {
-    window.stream.getTracks().forEach(track => {
+    window.stream.getTracks().forEach((track) => {
       track.stop();
     });
   }
@@ -93,7 +103,7 @@ function getStream() {
   const videoSource = videoSelect.value;
   const constraints = {
     audio: { deviceId: audioSource ? { exact: audioSource } : undefined },
-    video: { deviceId: videoSource ? { exact: videoSource } : undefined }
+    video: { deviceId: videoSource ? { exact: videoSource } : undefined },
   };
   return navigator.mediaDevices
     .getUserMedia(constraints)
@@ -104,10 +114,10 @@ function getStream() {
 function gotStream(stream) {
   window.stream = stream;
   audioSelect.selectedIndex = [...audioSelect.options].findIndex(
-    option => option.text === stream.getAudioTracks()[0].label
+    (option) => option.text === stream.getAudioTracks()[0].label
   );
   videoSelect.selectedIndex = [...videoSelect.options].findIndex(
-    option => option.text === stream.getVideoTracks()[0].label
+    (option) => option.text === stream.getVideoTracks()[0].label
   );
   videoElement.srcObject = stream;
   socket.emit("broadcaster");
